49ed821220ce5333c70864accdd198286a5dee4e
Brian <Brian@Brian-PC.home>
2012-07-28 23:24:15 -0400

    Created header and footer scripts

---
+++ web/Scripts/Header and Footer.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function add_header()
+{
+    document.getElementById("header").innerHTML="Look at this great header!";
+}
+
+function add_footer()
+{
+    document.getElementById("footer").innerHTML="Look at this great footer!";
+}
