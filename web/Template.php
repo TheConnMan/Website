b8277af0872760b6e074b21a4bc06a5cd9c2a4cf
brian <brian@ubuntu.ubuntu-domain>
2012-09-02 16:40:38 -0400

    Testing PHP

--- web/Template.php
+++ web/Template.php
@@ -1,33 +1,6 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
-<!DOCTYPE html>
-<html>
-    <head>
-        <title></title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link type="text/css" rel="stylesheet" href="CSS.css">
-    </head>
-    <body>
-	<div class="header">
-	    <div id="header">
-		<?php include 'header.php'?>
-	    </div>
-	</div>
-        <div class="wrapper">
-            <div id="nav" style="text-align: center">
-		<?php include 'nav.php'?>
-	    </div>
-        
-            <div>TODO write content</div>
-
-        </div>
-        <div class="footer">
-            <div id="footer" style="text-align: center">
-		<?php include 'footer.php'?>
-	    </div>
-        </div>
-        <script type="text/javascript" defer="defer" src="Scripts/Header%20and%20Footer.js"></script>
-    </body>
-</html>
+<?php include("header.php"); ?>
+    <div class="mainContent">
+      <h1>Header in here</h1>
+      <p>Paragraph in here</p>
+    </div>
+<?php include("footer.php"); ?>
