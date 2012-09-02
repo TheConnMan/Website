2c7f06fa3682debe8eb06a5aff082850ad374334
brian <brian@ubuntu.ubuntu-domain>
2012-09-02 16:11:56 -0400

    Changed to php

--- web/Template.php
+++ web/Template.php
@@ -9,15 +9,24 @@ and open the template in the editor.
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link type="text/css" rel="stylesheet" href="CSS.css">
     </head>
-    <body onload="add_header(); add_footer()">
+    <body>
+	<div class="header">
+	    <div id="header">
+		<?php include 'header.php'?>
+	    </div>
+	</div>
         <div class="wrapper">
-            <div id="header" style="text-align: center"></div>
+            <div id="nav" style="text-align: center">
+		<?php include 'nav.php'?>
+	    </div>
         
             <div>TODO write content</div>
 
         </div>
         <div class="footer">
-            <div id="footer" style="text-align: center"></div>
+            <div id="footer" style="text-align: center">
+		<?php include 'footer.php'?>
+	    </div>
         </div>
         <script type="text/javascript" defer="defer" src="Scripts/Header%20and%20Footer.js"></script>
     </body>
