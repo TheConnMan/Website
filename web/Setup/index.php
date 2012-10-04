9d59b849c39bd5e1528265b04a0d08f871854843
brian <brian@ubuntu.ubuntu-domain>
2012-10-03 23:07:00 -0400

    Look at all those changes again!

--- web/Setup/index.php
+++ web/Setup/index.php
@@ -1,17 +1,43 @@
 <?php include("../Setup/preheader.php"); ?>
 <title>Home</title>
-<?php include("header.php"); ?>
-<h1 style="padding-bottom: 5px">Welcome to my website!</h1>
-<div style="overflow: auto">
-    <div class="col" style="float: left">
-	<p>
-	    This site is a staging site for projects that I've put together as well as a 
-	    way to learn html and php. Right now there's just connect four, but there 
-	    will be more to come.
-	</p>
-    </div>
-    <div class="col" style="float: right">
-	Here's the right side!
-    </div>
-</div>
-<?php include("footer.php"); ?>
+<?php include("../Setup/header.php"); ?>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<table width="970" border="0">
+    <tr>
+	<td id="leftcolumn">
+	    <div id="leftmenu">
+		<ul style="list-style: none">
+		    <h3 style="padding-top: 15px">At A Glance</h3>
+		    <li style="padding-top: 15px">
+			<a href="#connectfourlabel">Connect Four</a>
+			<ul>
+			    <li><a href="#backstory">Backstory</a></li>
+			    <li><a href="#actualcode">The Actual Code</a></li>
+			</ul>
+		    </li>
+		</ul>
+	    </div>
+	</td><td width="3%"></td>
+	<td id="rightcolumn">
+	    <div id="rightcontent">
+		<h1 style="text-align: center">Welcome to my website!</h1>
+		<div style="text-align: left">
+		    <div style="padding-top: 5px; padding-left: 5px">
+			<p>
+			    This site is a staging site for projects that I've put together as well as a 
+			    way to learn html and php. Right now there's just connect four, but there 
+			    will be more to come.
+			</p>
+		    </div>
+		</div>
+	    </div>
+	</td>
+    </tr>
+</table>
+<script>
+    $(document).ready(function(){
+	$("#leftmenu").height($("#rightcontent").height())
+	
+    });
+</script>
+<?php include("../Setup/footer.php"); ?>
